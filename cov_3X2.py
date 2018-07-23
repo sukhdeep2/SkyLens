5e3de311d9ea52a4916115d5dea9d9c8f8941574
sukhdeep1989 <sukhdeep1989@github.com>
2018-07-23 17:56:13 -0400

    bug fixes to get ggl+gg

--- cov_3X2.py
+++ cov_3X2.py
@@ -225,9 +225,9 @@ class cov_3X2():
         if self.SSV_cov:
             clz=self.Ang_PS.clz
             zs1=self.z_bins[tracers[0]][zs_indx[0]]
-            zs2=self.z_bins[tracers[0]][zs_indx[1]]
-            zs3=self.z_bins[tracers[0]][zs_indx[2]]
-            zs4=self.z_bins[tracers[0]][zs_indx[3]]
+            zs2=self.z_bins[tracers[1]][zs_indx[1]]
+            zs3=self.z_bins[tracers[2]][zs_indx[2]]
+            zs4=self.z_bins[tracers[3]][zs_indx[3]]
             sigma_win=self.cov_utils.sigma_win
 
             sig_cL=zs1['kernel_int']*zs2['kernel_int']
@@ -273,11 +273,14 @@ class cov_3X2():
                                             ,bin_utils=self.cl_bin_utils)
         return cl_b,cov_b
 
-    def combine_cl_tomo(self,cl_compute_dict={},tracers=[]):
-        cl_b={}
-        for (i,j) in self.corr_indxs[(tracers[0],tracers[1])]+self.cov_indxs:
+    def combine_cl_tomo(self,cl_compute_dict={},corr=None):
+        corr2=corr[::-1]
+        cl_b={corr:{},corr2:{}}
+
+        for (i,j) in self.corr_indxs[corr]+self.cov_indxs:
             clij=cl_compute_dict[(i,j)]
-            cl_b[(i,j)],cov_none=self.bin_cl_func(cl=clij,cov=None)
+            cl_b[corr][(i,j)],cov_none=self.bin_cl_func(cl=clij,cov=None)
+            cl_b[corr2][(j,i)]=cl_b[corr][(i,j)]
         return cl_b
 
 
@@ -293,7 +296,19 @@ class cov_3X2():
 
         l=self.l
 
-        tracers=[j for i in corrs for j in i]
+        #tracers=[j for i in corrs for j in i]
+        tracers=np.unique([j for i in corrs for j in i])
+
+        corrs2=corrs
+        if self.do_cov:#make sure we compute cl for all cross corrs necessary for covariance
+                        #FIXME: If corrs are gg and ll only, this will lead to uncessary gl. This
+                        #        is an unlikely use case though
+            corrs2=[]
+            for i in np.arange(len(tracers)):
+                for j in np.arange(i,len(tracers)):
+                    corrs2+=[(tracers[i],tracers[j])]
+
+        print(corrs2)
 
         if cosmo_h is None:
             cosmo_h=self.Ang_PS.PS.cosmo_h
@@ -319,7 +334,7 @@ class cov_3X2():
         cl={}
         cov={}
         cl_b={}
-        for corr in corrs:
+        for corr in corrs2:
             corr2=corr[::-1]
             cl[corr]={}
             cl[corr2]={}
@@ -331,8 +346,7 @@ class cov_3X2():
                                         pk_func=pk_func,corr=corr)
 
                 cl[corr2][(j,i)]=cl[corr][(i,j)]#useful in gaussian covariance calculation.
-            cl_b[corr]=delayed(self.combine_cl_tomo)(cl[corr],tracers=corr)
-            cl_b[corr2]=delayed(self.combine_cl_tomo)(cl[corr2],tracers=corr2) #may not be needed
+            cl_b=delayed(self.combine_cl_tomo)(cl[corr],corr=corr)
 
         if self.do_cov:
             for corr1 in corrs:
@@ -423,20 +437,10 @@ class cov_3X2():
         if cosmo_h is None:
             cosmo_h=self.Ang_PS.PS.cosmo_h
 
-        if self.do_cov:#make sure we compute cl for all cross corrs necessary for covariance
-                        #FIXME: If corrs are gg and ll only, this will lead to uncessary gl. This
-                        #        is an unlikely use case though
-            tracers=np.unique([j for i in corrs for j in i])
-            corrs2=[]
-            for i in np.arange(len(tracers)):
-                for j in np.arange(i,len(tracers)):
-                    corrs2+=[(tracers[i],tracers[j])]
-
-        print(corrs2)
         #Donot use delayed here. Leads to error/repeated calculations
         cls_tomo_nu=self.cl_tomo(cosmo_h=cosmo_h,cosmo_params=cosmo_params,
                             pk_params=pk_params,pk_func=pk_func,
-                            corrs=corrs2)
+                            corrs=corrs)
 
         cl=cls_tomo_nu['cl']
         cov_xi={}
@@ -543,18 +547,19 @@ class cov_3X2():
 
         cov_final=np.zeros((len(D_final),len(D_final)))#np.int(nD2*(nD2+1)/2)
 
-
+        indx0_c1=0
         for ic1 in np.arange(len(corrs)):
             corr1=corrs[ic1]
             indxs_1=self.corr_indxs[corr1]
             nc1=len(indxs_1)
-            indx0_c1=(ic1)*nc1*len_bins
+            # indx0_c1=(ic1)*nc1*len_bins
 
+            indx0_c2=indx0_c1
             for ic2 in np.arange(ic1,len(corrs)):
                 corr2=corrs[ic2]
                 indxs_2=self.corr_indxs[corr2]
                 nc2=len(indxs_2)
-                indx0_c2=(ic2)*nc2*len_bins
+                # indx0_c2=(ic2)*nc2*len_bins
 
                 corr=corr1+corr2
                 n_m1_m2_1=1
@@ -574,7 +579,7 @@ class cov_3X2():
                         indx0_m2=(im2)*nc2*len_bins
                         for i1 in np.arange(nc1):
                             start2=0
-                            if ic1==ic2:
+                            if corr1==corr2:
                                 start2=i1
                             for i2 in np.arange(start2,nc2):
                                 indx0_1=(i1)*len_bins
@@ -603,7 +608,8 @@ class cov_3X2():
                                     j=indx0_c2+indx0_2+indx0_m1
                                     cov_final[i:i+len_bins,j:j+len_bins]=cov_here.T
                                     cov_final[j:j+len_bins,i:i+len_bins]=cov_here
-
+                indx0_c2+=nc2*len_bins
+            indx0_c1+=nc1*len_bins
 
             # dat2=dat['cov']
             # for jD2 in np.arange(iD2,nD2):
