changes, power spectra running, wrong results, annoying bug leading to core dump

angular_power_spectra.py
@@ -3,6 +3,7 @@ import os,sys
 from power_spectra import *
 from hankel_transform import *
 from binning import *
+from torch_utils import *
 from astropy.constants import c,G
 from astropy import units as u
 import numpy as np
@@ -13,7 +14,7 @@ d2r=np.pi/180.
 c=c.to(u.km/u.second)
 
 class Angular_power_spectra():
-    def __init__(self,silence_camb=False,l=np.arange(2,2001),power_spectra_kwargs={},
+    def __init__(self,silence_camb=False,l=tc.arange(2,2001),power_spectra_kwargs={},
                 z_PS=None,nz_PS=100,log_z_PS=False,z_PS_max=None,logger=None,
                 SSV_cov=False,tracer='kappa',cov_utils=None):
         self.logger=logger
@@ -29,7 +30,7 @@ class Angular_power_spectra():
         self.cov_utils=cov_utils
         self.set_z_PS(z=z_PS,nz=nz_PS,log_z=log_z_PS,z_max=z_PS_max)
 
-        self.dz=np.gradient(self.z)
+        self.dz=tc_gradient(self.z)
 
     def set_z_PS(self,z=None,nz=10,log_z=False,z_max=None):
         """
@@ -44,12 +45,12 @@ class Angular_power_spectra():
 
         if z is None:
             if log_z:#bins for z_lens.
-                self.z=np.logspace(np.log10(max(z_min,1.e-4)),np.log10(z_max),nz)
+                self.z=tc.logspace(np.log10(max(z_min,1.e-4)),np.log10(z_max),nz)
             else:
-                self.z=np.linspace(z_min,z_max,nz)
+                self.z=tc.linspace(z_min,z_max,nz)
         else:
             self.z=z
-        self.dz=np.gradient(self.z)
+        self.dz=tc_gradient(self.z)
 
     def angular_power_z(self,z=None,pk_params=None,cosmo_h=None,
                     cosmo_params=None,pk_func=None):
@@ -70,28 +71,28 @@ class Angular_power_spectra():
 
         #XXX At some point this should be moved to power spectra, pk and SSV, especially if doing 3X2
         if self.PS.pk is None:
-            self.PS.get_pk(z=z,pk_params=pk_params,cosmo_params=cosmo_params)
-        cls=np.zeros((nz,nl),dtype='float32')#*u.Mpc#**2
-
+            self.PS.get_pk(z=np.array(z),pk_params=pk_params,cosmo_params=cosmo_params)
+        cls=tc.zeros((nz,nl),dtype=l.dtype,device=l.device)#*u.Mpc#**2
+        print('got pk')
         Rls=None #pk response functions, used for SSV calculations
         RKls=None
         cls_lin=None #cls from linear power spectra, to compute \delta_window for SSV
         if self.SSV_cov: #things needed to compute SSV cov
-            Rls=np.zeros((nz,nl),dtype='float32')
-            RKls=np.zeros((nz,nl),dtype='float32')
-            cls_lin=np.zeros((nz,nl),dtype='float32')#*u.Mpc#**2
+            Rls=tc.zeros((nz,nl),dtype=l.dtype)
+            RKls=tc.zeros((nz,nl),dtype=l.dtype)
+            cls_lin=tc.zeros((nz,nl),dtype=l.dtype)#*u.Mpc#**2
 
         cH=c/(cosmo_h.efunc(self.z)*cosmo_h.H0)
         cH=cH.value
 
         def k_to_l(l,lz,f_k): #take func from k to l space
             fk_int=interp1d(lz,f_k,bounds_error=False,fill_value=0)
-            return fk_int(l)
+            return tc.tensor(fk_int(l),dtype=l.dtype,device=l.device)
 
         kh=self.PS.kh
         pk=self.PS.pk
-        chi=cosmo_h.comoving_transverse_distance(z).value
-        for i in np.arange(nz):
+        chi=tc.tensor(cosmo_h.comoving_transverse_distance(z).value,dtype=l.dtype,device=l.device)
+        for i in tc.arange(nz):
             DC_i=chi[i] #cosmo_h.comoving_transverse_distance(z[i]).value#because camb k in h/mpc
             lz=kh*DC_i-0.5
             cls[i][:]+=k_to_l(l,lz,pk[i]/DC_i**2)

binning.py
@@ -1,6 +1,7 @@
 from scipy.interpolate import interp1d,interp2d,RectBivariateSpline
 import numpy as np
 import itertools
+from torch_utils import *
 
 class binning():
     def __init__(self,rmin=0.1,rmax=100,kmax=10,kmin=1.e-4,n_zeros=1000,n_zeros_step=1000,
@@ -13,24 +14,27 @@ class binning():
         bu['n_bins']=len(r_bins)-1
         bu['bin_indx']=np.digitize(r,r_bins)-1
 
-        binning_mat=np.zeros((len(r),bu['n_bins']))
-        for i in np.arange(len(r)):
+        binning_mat=tc.zeros((len(r),bu['n_bins']),dtype=r.dtype,device=r.device)
+        for i in tc.arange(len(r)):
             if bu['bin_indx'][i]<0 or bu['bin_indx'][i]>=bu['n_bins']:
                 continue
             binning_mat[i,bu['bin_indx'][i]]=1.
         bu['binning_mat']=binning_mat
 
-        r2=np.sort(np.unique(np.append(r,r_bins))) #this takes care of problems around bin edges
-        dr=np.gradient(r2)
-        r2_idx=[i for i in np.arange(len(r2)) if r2[i] in r]
+        r2=tc.sort(tc.unique(tc.cat((r,r_bins))))[0] #this takes care of problems around bin edges
+        dr=tc_gradient(r2)
+        r2_idx=[i for i in tc.arange(len(r2)) if r2[i] in r]
         dr=dr[r2_idx]
         bu['r_dr']=r**(r_dim-1)*dr
         bu['r_dr']*=wt
-        bu['norm']=np.dot(bu['r_dr'],binning_mat)
+        # bu['norm']=bu['r_dr'].mm(binning_mat)
+        bu['norm']=(binning_mat.transpose(1,0)*bu['r_dr']).sum(1)
 
-        x=np.logical_or(r_bins[1:]<=np.amin(r),r_bins[:-1]>=np.amax(r))
+        x=r_bins[1:]<=r.min()#+r_bins[:-1]>=r.max()
         bu['norm'][x]=np.inf
-#         print(bu['norm'])
+        x=r_bins[:-1]>=r.max()
+        bu['norm'][x]=np.inf #FIXME: This should be done in 1-2 lines
+
 
         if mat_dims is not None:
             bu['r_dr_m']={}
@@ -39,19 +43,20 @@ class binning():
             for ndim in mat_dims:
                 s1=ls[0]
                 s2=ls[0]
-                r_dr_m=np.copy(bu['r_dr'])
-                norm_m=np.copy(bu['norm'])
-                for i in np.arange(ndim-1):
+                r_dr_m=bu['r_dr'].clone()
+                norm_m=bu['norm'].clone()
+                for i in tc.arange(ndim-1):
                     s1=s2+','+ls[i+1]
                     s2+=ls[i+1]
-                    r_dr_m=np.einsum(s1+'->'+s2,r_dr_m,bu['r_dr'])#works ok for 2-d case
-                    norm_m=np.einsum(s1+'->'+s2,norm_m,bu['norm'])#works ok for 2-d case
+                    r_dr_m=tc.einsum(s1+'->'+s2,(r_dr_m,bu['r_dr']))#works ok for 2-d case
+                    norm_m=tc.einsum(s1+'->'+s2,(norm_m,bu['norm']))#works ok for 2-d case
                 bu['r_dr_m'][ndim]=r_dr_m
                 bu['norm_m'][ndim]=norm_m
         return bu
 
     def bin_1d(self,xi=[],bin_utils=None):
-        xi_b=np.dot(xi*bin_utils['r_dr'],bin_utils['binning_mat'])
+        xi_b=(bin_utils['binning_mat'].transpose(1,0)*bin_utils['r_dr']*xi).sum(1)
+        #np.dot(xi*bin_utils['r_dr'],bin_utils['binning_mat'])
         xi_b/=bin_utils['norm']
         return xi_b
 
@@ -59,7 +64,8 @@ class binning():
         #r_dr=bin_utils['r_dr']
         #cov_r_dr=cov*bin_utils['r_dr_m'][2]#np.outer(r_dr,r_dr)
         binning_mat=bin_utils['binning_mat']
-        cov_b=np.dot(binning_mat.T, np.dot(cov*bin_utils['r_dr_m'][2],binning_mat) )
+        # cov_b=np.dot(binning_mat.T, np.dot(cov*bin_utils['r_dr_m'][2],binning_mat) )
+        cov_b=binning_mat.transpose(1,0).mm((cov*bin_utils['r_dr_m'][2]).mm(binning_mat))
         cov_b/=bin_utils['norm_m'][2]
         return cov_b
 
@@ -75,10 +81,10 @@ class binning():
 
         mat_r_dr=mat*r_dr_m # same as cov_r_dr=cov*np.outer(r_dr,r_dr)
         norm_ijk=bin_utils['norm_m'][ndim]
-        for indxs in itertools.product(np.arange(min(bin_idx),n_bins),repeat=ndim):
+        for indxs in itertools.product(tc.arange(min(bin_idx),n_bins),repeat=ndim):
             x={}#np.zeros_like(mat_r_dr,dtype='bool')
             mat_t=[]
-            for nd in np.arange(ndim):
+            for nd in tc.arange(ndim):
                 slc = [slice(None)] * (ndim)
                 #x[nd]=bin_idx==indxs[nd]
                 slc[nd]=bin_idx==indxs[nd]
@@ -95,26 +101,26 @@ class binning():
 
 #more basic binning code for testing.
 def bin_cov(r=[],cov=[],r_bins=[]):
-    bin_center=np.sqrt(r_bins[1:]*r_bins[:-1])
+    bin_center=tc.sqrt(r_bins[1:]*r_bins[:-1])
     n_bins=len(bin_center)
-    cov_int=np.zeros((n_bins,n_bins),dtype='float64')
+    cov_int=tc.zeros((n_bins,n_bins))
     bin_idx=np.digitize(r,r_bins)-1
-    r2=np.sort(np.unique(np.append(r,r_bins))) #this takes care of problems around bin edges
-    dr=np.gradient(r2)
-    r2_idx=[i for i in np.arange(len(r2)) if r2[i] in r]
+    r2=tc.sort(tc.unique(tc.cat(r,r_bins)))[0] #this takes care of problems around bin edges
+    dr=tc_gradient(r2)
+    r2_idx=[i for i in tc.arange(len(r2)) if r2[i] in r]
     dr=dr[r2_idx]
     r_dr=r*dr
-    cov_r_dr=cov*np.outer(r_dr,r_dr)
-    for i in np.arange(min(bin_idx+1),n_bins):
+    cov_r_dr=cov*tc.ger(r_dr,r_dr)
+    for i in tc.arange(min(bin_idx+1),n_bins):
         xi=bin_idx==i
-        for j in np.arange(min(bin_idx),n_bins):
+        for j in tc.arange(min(bin_idx),n_bins):
             xj=bin_idx==j
-            norm_ij=np.sum(r_dr[xi])*np.sum(r_dr[xj])
+            norm_ij=tc.sum(r_dr[xi])*tc.sum(r_dr[xj])
             if i==j:
                 print( i,j,norm_ij)
             if norm_ij==0:
                 continue
-            cov_int[i][j]=np.sum(cov_r_dr[xi,:][:,xj])/norm_ij
+            cov_int[i][j]=tc.sum(cov_r_dr[xi,:][:,xj])/norm_ij
     #cov_int=np.nan_to_num(cov_int)
 #         print np.diag(cov_r_dr)
     return cov_int

cov_3X2.py
@@ -13,6 +13,7 @@ from galaxy_utils import *
 from astropy.constants import c,G
 from astropy import units as u
 import numpy as np
+import torch as tc
 from scipy.interpolate import interp1d
 import warnings,logging
 
@@ -32,8 +33,10 @@ class cov_3X2():
                 use_window=True,
                 sigma_gamma=0.3,f_sky=0.3,l_bins=None,bin_cl=False,
                 stack_data=False,bin_xi=False,do_xi=False,theta_bins=None,
+                tc_device=None,tc_dtype=tc.double,
                 corrs=[('shear','shear')]):
-        self.logger=logger
+        kwargs=locals()
+        self.__dict__.update(**kwargs)
         self.cov_SSC_nobin={}
         if logger is None:
             self.logger=logging.getLogger()
@@ -45,28 +48,19 @@ class cov_3X2():
             # ch.setFormatter(format)
             # self.logger.addHandler(ch)
 
-        self.do_cov=do_cov
-        self.SSV_cov=SSV_cov
-        self.tidal_SSV_cov=tidal_SSV_cov
-        self.l=l
-        self.do_xi=do_xi
-        self.corrs=corrs
         self.l_cut_jnu=None
         if do_xi:
             self.set_HT(HT=HT,HT_kwargs=HT_kwargs)
 
-        self.lensing_utils=lensing_utils
         if lensing_utils is None:
             self.lensing_utils=Lensing_utils(sigma_gamma=sigma_gamma,zs_bins=zs_bins,
                                             logger=self.logger)
 
-        self.cov_utils=cov_utils
         if cov_utils is None:
             self.cov_utils=Covariance_utils(f_sky=f_sky,l=self.l,logger=self.logger,
                                             l_cut_jnu=self.l_cut_jnu,
                                             do_sample_variance=do_sample_variance,use_window=use_window)
 
-        self.Ang_PS=Ang_PS
         if Ang_PS is None:
             self.Ang_PS=Angular_power_spectra(silence_camb=silence_camb,
                                 SSV_cov=SSV_cov,l=self.l,logger=self.logger,
@@ -76,7 +70,6 @@ class cov_3X2():
                                 z_PS_max=z_PS_max)
                         #FIXME: Need a dict for these args
 
-        self.galaxy_utils=galaxy_utils
         if galaxy_utils is None:
             self.galaxy_utils=Galaxy_utils(zg_bins=zg_bins,logger=self.logger,l=self.l,
                                             z_th=self.Ang_PS.z)
@@ -84,13 +77,8 @@ class cov_3X2():
         self.z_bins['shear']=self.lensing_utils.zs_bins
         #self.z_bins['kappa']=self.lensing_utils.zk_bins
         self.z_bins['galaxy']=self.galaxy_utils.zg_bins
-        self.l_bins=l_bins
-        self.stack_data=stack_data
-        self.theta_bins=theta_bins
-        self.bin_utils=None
 
-        self.bin_cl=bin_cl
-        self.bin_xi=bin_xi
+        self.bin_utils=None
         self.set_bin_params()
         self.cov_indxs=[]
         self.corr_indxs={}
@@ -171,8 +159,8 @@ class cov_3X2():
         f=self.Ang_PS.cl_f
         sc=zs1['kernel_int']*zs2['kernel_int']
 
-        # cl=np.dot(sc*clz['dchi'],cls)
-        cl=np.dot(cls.T*sc,clz['dchi'])
+        # cl=np.dot(cls.T*sc,clz['dchi'])
+        cl=(cls.transpose(1,0)*sc*clz['dchi']).sum(1)
 
         cl/=self.Ang_PS.cl_f**2# cl correction from Kilbinger+ 2017
         # cl*=2./np.pi #FIXME: needed to match camb... but not CCL
@@ -180,7 +168,7 @@ class cov_3X2():
 
 #     @jit
     def get_cl(self,zs1_indx=-1, zs2_indx=-1,corr=('shear','shear'),
-                pk_func=None,pk_params=None,cosmo_h=None,cosmo_params=None):
+                ):
         """
             Wrapper for calc_lens_lens_cl. Checks to make sure quantities such as power spectra and cosmology
             are available otherwise sets them to some default values.
@@ -235,7 +223,7 @@ class cov_3X2():
             sig_cL=zs1['kernel_int']*zs2['kernel_int']*zs3['kernel_int']*zs4['kernel_int']
             # sig_cL*=zs3['kernel_int']*zs4['kernel_int']
 
-            sig_cL*=self.Ang_PS.clz['dchi']
+            sig_cL=sig_cL*self.Ang_PS.clz['dchi']
 
             if self.do_xi:
                 cov['kernel']=sig_cL
@@ -248,7 +236,7 @@ class cov_3X2():
                 clr+=self.Ang_PS.clz['clsRK']/6.
 
             # cov['SSC_dd']=np.dot((clr1).T*sig_cL,clr1)
-            cov['SSC']=np.dot((clr).T*sig_cL,clr)
+            cov['SSC']=(clr.transpose(1,0)*sig_cL).mm(clr)#np.dot((clr).T*sig_cL,clr)
             cov['final']+=cov['SSC']
 
         for k in ['G','SSC','final']:#no need to bin G1324 and G1423
@@ -309,8 +297,6 @@ class cov_3X2():
                 for j in np.arange(i,len(tracers)):
                     corrs2+=[(tracers[i],tracers[j])]
 
-        print(corrs2)
-
         if cosmo_h is None:
             cosmo_h=self.Ang_PS.PS.cosmo_h
 
@@ -342,9 +328,7 @@ class cov_3X2():
             corr_indxs=self.corr_indxs[(corr[0],corr[1])]#+self.cov_indxs
             for (i,j) in corr_indxs:
                 # out[(i,j)]
-                cl[corr][(i,j)]=delayed(self.get_cl)(zs1_indx=i,zs2_indx=j,cosmo_h=cosmo_h,
-                                        cosmo_params=cosmo_params,pk_params=pk_params,
-                                        pk_func=pk_func,corr=corr)
+                cl[corr][(i,j)]=delayed(self.get_cl)(zs1_indx=i,zs2_indx=j,corr=corr)
 
                 cl[corr2][(j,i)]=cl[corr][(i,j)]#useful in gaussian covariance calculation.
             cl_b=delayed(self.combine_cl_tomo)(cl[corr],corr=corr)
@@ -380,7 +364,7 @@ class cov_3X2():
 
         if self.HT.name=='Hankel' and m1_m2!=m1_m2_cross:
             n=len(self.theta_bins)-1
-            cov_xi['final']=np.zeros((n,n))
+            cov_xi['final']=tc.zeros((n,n))
             return cov_xi
 
         Norm= self.cov_utils.Om_W
@@ -531,7 +515,7 @@ class cov_3X2():
                 n_m1_m2=len(self.m1_m2s[corr])
             n_bins+=len(self.corr_indxs[corr])*n_m1_m2 #np.int64(nbins*(nbins-1.)/2.+nbins)
         print(n_bins,len_bins,n_m1_m2)
-        D_final=np.zeros(n_bins*len_bins)
+        D_final=tc.zeros(n_bins*len_bins)
 
         i=0
         for corr in corrs:
@@ -546,11 +530,10 @@ class cov_3X2():
                     dat_c=dat[est][corr][m1_m2[im]]
 
                 for indx in self.corr_indxs[corr]:
-                    # print(len_bins,dat_c[indx].shape)
                     D_final[i*len_bins:(i+1)*len_bins]=dat_c[indx]
                     i+=1
 
-        cov_final=np.zeros((len(D_final),len(D_final)))-999.#np.int(nD2*(nD2+1)/2)
+        cov_final=tc.zeros((len(D_final),len(D_final)))-999.#np.int(nD2*(nD2+1)/2)
 
         indx0_c1=0
         for ic1 in np.arange(len(corrs)):
@@ -594,6 +577,7 @@ class cov_3X2():
                                 if self.do_xi:
                                     cov_here=dat['cov'][corr][m1_m2_1[im1]+m1_m2_2[im2]][indx]['final']
                                 else:
+                                    print(dat['cov'].keys())
                                     cov_here=dat['cov'][corr][indx]['final']
 
                                 # if im1==im2:
@@ -601,7 +585,7 @@ class cov_3X2():
                                 j=indx0_c2+indx0_2+indx0_m2
 
                                 cov_final[i:i+len_bins,j:j+len_bins]=cov_here
-                                cov_final[j:j+len_bins,i:i+len_bins]=cov_here.T
+                                cov_final[j:j+len_bins,i:i+len_bins]=cov_here.transpose(1,0)
 
                                 if im1!=im2 and corr1==corr2:
                                     # i=indx0_c1+indx0_1+indx0_m1
@@ -611,7 +595,7 @@ class cov_3X2():
 
                                     i=indx0_c1+indx0_1+indx0_m2
                                     j=indx0_c2+indx0_2+indx0_m1
-                                    cov_final[i:i+len_bins,j:j+len_bins]=cov_here.T
+                                    cov_final[i:i+len_bins,j:j+len_bins]=cov_here.transpose(1,0)
                                     cov_final[j:j+len_bins,i:i+len_bins]=cov_here
 
                 indx0_c2+=n_indx2*len_bins*n_m1_m2_2

cov_utils.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.interpolate import interp1d
 from scipy.integrate import quad as scipy_int1d
 from scipy.special import jn, jn_zeros
-
+from torch_utils import *
 
 d2r=np.pi/180.
 sky_area=np.pi*4/(d2r)**2 #in degrees
@@ -18,14 +18,14 @@ class Covariance_utils():
         self.f_sky=f_sky
 
         self.use_window=use_window
-        self.sample_variance_f=1
+        self.sample_variance_f=tc.tensor([1.],dtype=l.dtype,device=l.device)
         if not do_sample_variance:
-            self.sample_variance_f=0 #remove sample_variance from gaussian part
+            self.sample_variance_f*=0 #remove sample_variance from gaussian part
 
         self.set_window_params(f_sky=self.f_sky)
         self.window_func(theta_win=self.theta_win,f_sky=self.f_sky)
 
-        self.gaussian_cov_norm=(2.*l+1.)*f_sky*np.gradient(l) #need Delta l here. Even when
+        self.gaussian_cov_norm=(2.*l+1.)*f_sky*tc_gradient(l) #need Delta l here. Even when
                                                                     #binning later
 
     def set_window_params(self,f_sky=None):
@@ -46,17 +46,18 @@ class Covariance_utils():
 
     def sigma_win_calc(self,cls_lin):
         if self.l_cut_jnu is None:
-            self.sigma_win=np.dot(self.Win**2*np.gradient(self.l)*self.l,cls_lin.T)
+            #self.sigma_win=np.dot(self.Win**2*tc_gradient(self.l)*self.l,cls_lin.transpose(1,0))
+            self.sigma_win=(self.Win**2*tc_gradient(self.l)*self.l*cls_lin).sum(1)
         else: #FIXME: This is ugly. Only needed for hankel transform (not wigner). Remove if HT is deprecated.
             self.sigma_win={}
             for m1_m2 in self.l_cut_jnu['m1_m2s']:
                 lc=self.l_cut_jnu[m1_m2]
-                self.sigma_win[m1_m2]=np.dot(self.Win[lc]**2*np.gradient(self.l[lc])*self.l[lc],cls_lin[:,lc].T)
+                self.sigma_win[m1_m2]=np.dot(self.Win[lc]**2*tc_gradient(self.l[lc])*self.l[lc],cls_lin[:,lc])
         #FIXME: This is ugly
 
     def corr_matrix(self,cov=[]):
-        diag=np.diag(cov)
-        return cov/np.sqrt(np.outer(diag,diag))
+        diag=cov.diag()
+        return cov/tc.sqrt(tc.ger(diag,diag))
 
 
     def gaussian_cov_auto(self,cls,SN,tracers,z_indx,do_xi):
@@ -70,27 +71,31 @@ class Covariance_utils():
         # print(cls[(tracers[0],tracers[2])].keys())
         G1324= ( cls[(tracers[0],tracers[2])] [(z_indx[0], z_indx[2]) ]*self.sample_variance_f
              # + (SN.get((tracers[0],tracers[2]))[:,z_indx[0], z_indx[2] ]  or 0)
-             + (SN[(tracers[0],tracers[2])][:,z_indx[0], z_indx[2] ] if SN.get((tracers[0],tracers[2])) is not None else 0)
+             + (SN[(tracers[0],tracers[2])][:,z_indx[0], z_indx[2] ] if SN.get((tracers[0],tracers[2])) is not None else
+                                                                                                        tc.tensor(0,dtype=tc.double))
                 )
              #get returns None if key doesnot exist. or 0 adds 0 is SN is none
 
         G1324*=( cls[(tracers[1],tracers[3])][(z_indx[1], z_indx[3]) ]*self.sample_variance_f
               # +(SN.get((tracers[1],tracers[3]))[:,z_indx[1], z_indx[3] ] or 0)
-              + (SN[(tracers[1],tracers[3])][:,z_indx[1], z_indx[3] ] if SN.get((tracers[1],tracers[3])) is not None else 0)
+              + (SN[(tracers[1],tracers[3])][:,z_indx[1], z_indx[3] ] if SN.get((tracers[1],tracers[3])) is not None else
+                                                                                                                tc.tensor(0,dtype=tc.double))
               )
 
         G1423= ( cls[(tracers[0],tracers[3])][(z_indx[0], z_indx[3]) ]*self.sample_variance_f
               # + (SN.get((tracers[0],tracers[3]))[:,z_indx[0], z_indx[3] ] or 0)
-              + (SN[(tracers[0],tracers[3])][:,z_indx[0], z_indx[3] ] if SN.get((tracers[0],tracers[3])) is not None else 0)
+              + (SN[(tracers[0],tracers[3])][:,z_indx[0], z_indx[3] ] if SN.get((tracers[0],tracers[3])) is not None else
+                                                                                                                tc.tensor(0,dtype=tc.double))
               )
 
         G1423*=( cls[(tracers[1],tracers[2])][(z_indx[1], z_indx[2]) ]*self.sample_variance_f
              # + (SN.get((tracers[1],tracers[2]))[:,z_indx[1], z_indx[2] ] or 0)
-             + (SN[(tracers[1],tracers[2])][:,z_indx[1], z_indx[2] ] if SN.get((tracers[1],tracers[2])) is not None else 0)
+             + (SN[(tracers[1],tracers[2])][:,z_indx[1], z_indx[2] ] if SN.get((tracers[1],tracers[2])) is not None else
+                                                                                                                tc.tensor(0,dtype=tc.double))
                 )
 
         G=None
         if not do_xi:
-            G=np.diag(G1423+G1324)
+            G=(G1423+G1324).diag()
             G/=self.gaussian_cov_norm
         return G,G1324,G1423

hankel_transform.py
@@ -2,6 +2,7 @@ from scipy.special import jn, jn_zeros,jv
 from scipy.interpolate import interp1d,interp2d,RectBivariateSpline
 from scipy.optimize import fsolve
 import numpy as np
+import torch as tc
 import itertools
 
 class hankel_transform():
@@ -67,8 +68,12 @@ class hankel_transform():
             theta=theta[idx]
             self.logger.info ('pruned theta:%s',len(theta))
         theta=np.unique(theta)
+
+        l=tc.tensor(l,dtype=tc.double)
+        theta=tc.tensor(theta,dtype=tc.double)
+        print(theta)
         self.logger.info ('nr:%s',len(theta))
-        J=jn(j_nu,np.outer(theta,l))
+        J=jn(j_nu,tc.ger(theta,l))
         J_nu1=jn(j_nu+1,zeros)
         return l,l_max,theta,J,J_nu1,zeros
 
@@ -86,9 +91,9 @@ class hankel_transform():
 
     def _cl_cov_grid(self,l_cl=[],cl_cov=[],m1_m2=[],taper=False,**kwargs):
         if taper:#FIXME there is no check on change in taper_kwargs
-            if self.taper_f2 is None or not np.all(np.isclose(self.taper_f['l'],l_cl)):
+            if self.taper_f2 is None or not tc.all(tc.isclose(self.taper_f['l'],l_cl)):
                 self.taper_f=self.taper(l=l_cl,**kwargs)
-                taper_f2=np.outer(self.taper_f['taper_f'],self.taper_f['taper_f'])
+                taper_f2=tc.ger(self.taper_f['taper_f'],self.taper_f['taper_f'])
                 self.taper_f2={'l':l_cl,'taper_f2':taper_f2}
             cl_cov=cl_cov*self.taper_f2['taper_f2']
         if l_cl==[]:#In this case pass a function that takes k with kwargs and outputs cl
@@ -102,7 +107,7 @@ class hankel_transform():
 
     def projected_correlation(self,l_cl=[],cl=[],m1_m2=[],taper=False,**kwargs):
         cl2=self._cl_grid(l_cl=l_cl,cl=cl,m1_m2=m1_m2,taper=taper,**kwargs)
-        w=np.dot(self.J[m1_m2],cl2/self.J_nu1[m1_m2]**2)
+        w=(self.J[m1_m2]*cl2/self.J_nu1[m1_m2]**2).sum(0)
         w*=(2.*self.l_max[m1_m2]**2/self.zeros[m1_m2][-1]**2)/(2*np.pi)
         return self.theta[m1_m2],w
 
@@ -110,7 +115,7 @@ class hankel_transform():
     #we will use relation spherical_jn(z)=j{n+0.5}(z)*sqrt(pi/2z)
     #cl will be written as k*cl
         cl2=self._cl_grid(l_cl=l_cl,cl=cl,m1_m2=m1_m2,taper=taper,**kwargs)
-        j_f=np.sqrt(np.pi/2./np.outer(self.theta[m1_m2],self.l[m1_m2]))
+        j_f=tc.sqrt(np.pi/2./tc.ger(self.theta[m1_m2],self.l[m1_m2]))
         w=np.dot(self.J[m1_m2],cl2*self.l[m1_m2]/self.J_nu1[m1_m2]**2)
         w*=(2.*self.l_max[m1_m2]**2/self.zeros[m1_m2][-1]**2)/(2*np.pi)
         return self.theta[m1_m2],w
@@ -119,7 +124,7 @@ class hankel_transform():
         #when cl_cov can be written as vector, eg. gaussian covariance
         cl1=self._cl_grid(l_cl=l_cl,cl=cl_cov,m1_m2=m1_m2,taper=taper,**kwargs)
         # cov=np.dot(self.J[m1_m2],(self.J[m1_m2]*cl1*cl2/self.J_nu1[m1_m2]**2).T)
-        cov=np.einsum('rk,k,sk->rs',self.J[m1_m2],cl1/self.J_nu1[m1_m2]**2,
+        cov=tc.einsum('rk,k,sk->rs',self.J[m1_m2],cl1/self.J_nu1[m1_m2]**2,
                     self.J[m1_m2],optimize=True)
         cov*=(2.*self.l_max[m1_m2]**2/self.zeros[m1_m2][-1]**2)/(2*np.pi)
         return self.theta[m1_m2],cov
@@ -127,31 +132,31 @@ class hankel_transform():
     def projected_covariance2(self,l_cl=[],cl_cov=[],m1_m2=[],taper=False,**kwargs):
         #when cl_cov is a 2-d matrix
         cl_cov2=cl_cov#self._cl_cov_grid(l_cl=l_cl,cl_cov=cl_cov,m1_m2=m1_m2,taper=taper,**kwargs)
-        cov=np.dot(self.J[m1_m2],np.dot(self.J[m1_m2]/self.J_nu1[m1_m2]**2,cl_cov2).T)
+        cov=tc.dot(self.J[m1_m2],tc.dot(self.J[m1_m2]/self.J_nu1[m1_m2]**2,cl_cov2).T)
         cov*=(2.*self.l_max[m1_m2]**2/self.zeros[m1_m2][-1]**2)/(2*np.pi)
         return self.theta[m1_m2],cov
 
     def taper(self,l=[],large_k_lower=10,large_k_upper=100,low_k_lower=0,low_k_upper=1.e-5):
         #FIXME there is no check on change in taper_kwargs
-        if self.taper_f is None or not np.all(np.isclose(self.taper_f['l'],k)):
-            taper_f=np.zeros_like(l)
+        if self.taper_f is None or not tc.all(tc.isclose(self.taper_f['l'],k)):
+            taper_f=tc.zeros_like(l)
             x=k>large_k_lower
-            taper_f[x]=np.cos((k[x]-large_k_lower)/(large_k_upper-large_k_lower)*np.pi/2.)
+            taper_f[x]=tc.cos((k[x]-large_k_lower)/(large_k_upper-large_k_lower)*np.pi/2.)
             x=k<large_k_lower and k>low_k_upper
             taper_f[x]=1
             x=k<low_k_upper
-            taper_f[x]=np.cos((k[x]-low_k_upper)/(low_k_upper-low_k_lower)*np.pi/2.)
+            taper_f[x]=tc.cos((k[x]-low_k_upper)/(low_k_upper-low_k_lower)*np.pi/2.)
             self.taper_f={'taper_f':taper_f,'l':k}
         return self.taper_f
 
     def diagonal_err(self,cov=[]):
-        return np.sqrt(np.diagonal(cov))
+        return tc.sqrt(tc.diagonal(cov))
 
     def skewness(self,l_cl=[],cl1=[],cl2=[],cl3=[],m1_m2=[],taper=False,**kwargs):
         cl1=self._cl_grid(l_cl=l_cl,cl=cl1,m1_m2=m1_m2,taper=taper,**kwargs)
         cl2=self._cl_grid(l_cl=l_cl,cl=cl2,m1_m2=m1_m2,taper=taper,**kwargs)
         cl3=self._cl_grid(l_cl=l_cl,cl=cl3,m1_m2=m1_m2,taper=taper,**kwargs)
-        skew=np.einsum('ji,ki,li',self.J[m1_m2],self.J[m1_m2],
+        skew=tc.einsum('ji,ki,li',self.J[m1_m2],self.J[m1_m2],
                         self.J[m1_m2]*cl1*cl2*cl3/self.J_nu1[m1_m2]**2)
         skew*=(2.*self.l_max[m1_m2]**2/self.zeros[m1_m2][-1]**2)/(2*np.pi)
         return self.theta[m1_m2],skew

lensing_utils.py
@@ -7,6 +7,7 @@ from binning import *
 from astropy.constants import c,G
 from astropy import units as u
 import numpy as np
+import torch as tc
 from scipy.interpolate import interp1d
 from scipy.integrate import quad as scipy_int1d
 
@@ -32,28 +33,28 @@ class Lensing_utils():
         #rc=3*cosmo_h.H0**2/(8*np.pi*G2)
         rc=cosmo_h.H0**2/(self.G2) #factors of pi etc. absorbed in self.G2
         rc=rc.to(u.Msun/u.pc**2/u.Mpc)# unit of Msun/pc^2/mpc
-        return rc.value
+        return tc.tensor(rc.value,dtype=tc.double)
 
     def sigma_crit(self,zl=[],zs=[],cosmo_h=None):
-        ds=cosmo_h.comoving_transverse_distance(zs)
-        dl=cosmo_h.comoving_transverse_distance(zl)
-        ddls=1.-np.multiply.outer(1./ds,dl)#(ds-dl)/ds
+        ds=tc.tensor(cosmo_h.comoving_transverse_distance(zs))
+        dl=tc.tensor(cosmo_h.comoving_transverse_distance(zl))
+        ddls=1.-tc.ger(1./ds,dl) #(ds-dl)/ds
         w=(3./2.)*((cosmo_h.H0/c)**2)*(1+zl)*dl/self.Rho_crit(cosmo_h)
-        sigma_c=1./(ddls*w)
+        sigma_c=1./(ddls*tc.tensor(w))
         x=ddls<=0 #zs<zl
         sigma_c[x]=np.inf
-        return sigma_c.value
+        return sigma_c
 
     def shape_noise_calc(self,zs1=None,zs2=None):
-        if not np.array_equal(zs1['z'],zs2['z']):
+        if not tc.all(zs1['z'].eq(zs2['z'])):
             return 0
-        if np.any(np.isinf(zs1['nz'])) or np.any(np.isinf(zs2['nz'])):
+        if tc.any(zs1['nz']==float('inf')) or tc.any(zs2['nz']==float('inf')):
             return 0
-        SN=self.SN0*np.sum(zs1['W']*zs2['W']*zs1['nz']) #FIXME: this is probably wrong.
+        SN=self.SN0*(zs1['W']*zs2['W']*zs1['nz']).sum() #FIXME: this is probably wrong.
         #Assumption: ns(z)=ns*pzs*dzs
-        SN/=np.sum(zs1['nz']*zs1['W'])
-        SN/=np.sum(zs2['nz']*zs2['W'])
-        return SN
+        SN/=(zs1['nz']*zs1['W']).sum()
+        SN/=(zs2['nz']*zs2['W']).sum()
+        return tc.tensor(SN,dtype=tc.double)
         # XXX Make sure pzs are properly normalized
 
 
@@ -69,16 +70,17 @@ class Lensing_utils():
             ns: The number density for each bin to compute shape noise.
         """
         self.ns_bins=self.zs_bins['n_bins']
-        self.SN=np.zeros((1,self.ns_bins,self.ns_bins)) #if self.do_cov else None
+        self.SN=tc.zeros((1,self.ns_bins,self.ns_bins),dtype=tc.double) #if self.do_cov else None
 
-        for i in np.arange(self.ns_bins):
+        for i in tc.arange(self.ns_bins):
+            i=i.item()
             self.zs_bins[i]['SN']=self.shape_noise_calc(zs1=self.zs_bins[i],
                                                                     zs2=self.zs_bins[i])
             self.SN[:,i,i]=self.zs_bins[i]['SN']
 
         if not cross_PS: # if we are not computing cross_PS, then we assume that sources overlap in different bins. Hence we need to compute the cross shape noise
-            for i in np.arange(self.ns_bins):
-                for j in np.arange(i,self.ns_bins):
+            for i in tc.arange(self.ns_bins):
+                for j in tc.arange(i,self.ns_bins):
                     self.SN[:,i,j]=self.shape_noise_calc(zs1=self.zs_bins[i],
                                                                         zs2=self.zs_bins[j])
                     self.SN[:,j,i]=self.SN[:,i,j]
@@ -94,17 +96,18 @@ class Lensing_utils():
 
         rho=self.Rho_crit(cosmo_h=cosmo_h)*cosmo_h.Om0
 
-        for i in np.arange(self.ns_bins):
+        for i in tc.arange(self.ns_bins):
+            i=i.item()
             self.zs_bins[i]['kernel']=rho/self.sigma_crit(zl=zl,
                                                         zs=self.zs_bins[i]['z'],
                                                         cosmo_h=cosmo_h)
-            self.zs_bins[i]['kernel_int']=np.dot(self.zs_bins[i]['pzdz'],self.zs_bins[i]['kernel'])
+            self.zs_bins[i]['kernel_int']=(self.zs_bins[i]['pzdz']*self.zs_bins[i]['kernel'].transpose(1,0)).sum(1)
             self.zs_bins[i]['kernel_int']/=self.zs_bins[i]['Norm']
 
     def reset_zs(self):
         """
             Reset cosmology dependent values for each source bin
         """
-        for i in np.arange(self.ns_bins):
+        for i in tc.arange(self.ns_bins):
             self.zs_bins[i]['kernel']=None
             self.zs_bins[i]['kernel_int']=None

lsst_utils.py
@@ -1,6 +1,7 @@
 from scipy.stats import norm as gaussian
 import copy
 import numpy as np
+import torch as tc
 from lensing_utils import *
 from astropy.cosmology import Planck15 as cosmo
 from astropy.table import Table
@@ -8,9 +9,9 @@ cosmo_h_PL=cosmo.clone(H0=100)
 
 
 def lsst_pz_source(alpha=1.24,z0=0.51,beta=1.01,z=[]):
-    p_zs=z**alpha*np.exp(-(z/z0)**beta)
-    p_zs/=np.sum(np.gradient(z)*p_zs)
-    return p_zs
+    p_zs=z**alpha*tc.exp(-(z/z0)**beta)
+    p_zs/=(tc_gradient(z)*p_zs).sum()
+    return tc.tensor(p_zs,dtype=tc.double)
 
 def ztrue_given_pz_Gaussian(zp=[],p_zp=[],bias=[],sigma=[],zs=None,ns=0):
     """
@@ -24,24 +25,20 @@ def ztrue_given_pz_Gaussian(zp=[],p_zp=[],bias=[],sigma=[],zs=None,ns=0):
         ns: Source number density. Needed to return n(zs)
     """
     if zs is None:
-        zs=np.linspace(min(zp-sigma*5),max(zp-sigma*5),500)
+        zs=tc.linspace(min(zp-sigma*5),max(zp-sigma*5),500,dtype=tc.double)
 
-    y=np.tile(zs, (len(zp),1)  )
-    pdf=gaussian.pdf(y.T,loc=zp+bias,scale=sigma).T
-    # pdf=np.zeros((len(zp),len(zs)))
-    # for i in np.arange(len(zp)):
-    #     pdf[i,:]=gaussian.pdf(zs,loc=zp[i]+bias[i],scale=sigma[i])
+    y=tc.tensor(np.tile(zs, (len(zp),1)  )) #FIXME
+    pdf=tc.tensor(gaussian.pdf(y.transpose(1,0),loc=zp+bias,scale=sigma),dtype=p_zp.dtype)#.transpose(1,0)
 
-    dzp=np.gradient(zp)
+    dzp=tc_gradient(zp)
+    p_zs=(dzp*p_zp*pdf).sum(1)
 
-    p_zs=np.dot(dzp*p_zp,pdf)
-
-    dzs=np.gradient(zs)
-    p_zs/=np.sum(p_zs*dzs)
+    dzs=tc_gradient(zs)
+    p_zs/=(p_zs*dzs).sum()
     nz=dzs*p_zs*ns
     return zs,p_zs,nz
 
-def source_tomo_bins(zp=None,p_zp=None,nz_bins=None,ns=26,ztrue_func=None,zp_bias=None,
+def source_tomo_bins(zp=None,p_zp=None,nz_bins=None,ns=26.,ztrue_func=None,zp_bias=None,
                     zp_sigma=None,zs=None):
     """
         Setting source redshift bins in the format used in code.
@@ -57,44 +54,46 @@ def source_tomo_bins(zp=None,p_zp=None,nz_bins=None,ns=26,ztrue_func=None,zp_bia
 
     if nz_bins is None:
         nz_bins=1
-    z_bins=np.linspace(min(zp)-0.0001,max(zp)+0.0001,nz_bins+1)
+    z_bins=tc.linspace(min(zp)-0.0001,max(zp)+0.0001,nz_bins+1)
 
     if zs is None:
-        zs=np.linspace(0,2.5,100)
-    dzs=np.gradient(zs)
-    dzp=np.gradient(zp) if len(zp)>1 else [1]
-    zp=np.array(zp)
+        zs=tc.linspace(0,2.5,100,dtype=tc.double)
+    dzs=tc_gradient(zs)
+    dzp=tc_gradient(zp) if len(zp)>1 else tc.tensor([1],dtype=tc.double) #FIXME
+    zp=tc.tensor(zp,dtype=tc.double)
 
-    zl_kernel=np.linspace(0,2,50)
+    zl_kernel=tc.linspace(0,2,50)
     lu=Lensing_utils()
     cosmo_h=cosmo_h_PL
 
-    for i in np.arange(nz_bins):
+    for i in tc.arange(nz_bins):
+        i=i.item()
         zs_bins[i]={}
-        indx=zp.searchsorted(z_bins[i:i+2])
+        indx=np.searchsorted(zp,z_bins[i:i+2]) #search for nearest values to bin edges
 
         if ztrue_func is None:
-            if indx[0]==indx[1]:
+            if indx[1]==indx[0]:
                 indx[1]=-1
             zs=zp[indx[0]:indx[1]]
             p_zs=p_zp[indx[0]:indx[1]]
-            nz=ns*p_zs*dzp[indx[0]:indx[1]]
+            print(ns,p_zs,dzp)
+            nz=(ns*p_zs*dzp[indx[0]:indx[1]]).type(tc.double)
             print(indx,zs,z_bins)
         else:
-            ns_i=ns*np.sum(p_zp[indx[0]:indx[1]]*dzp[indx[0]:indx[1]])
+            ns_i=ns*(p_zp[indx[0]:indx[1]]*dzp[indx[0]:indx[1]]).sum()
             zs,p_zs,nz=ztrue_func(zp=zp[indx[0]:indx[1]],p_zp=p_zp[indx[0]:indx[1]],
                             bias=zp_bias[indx[0]:indx[1]],
                             sigma=zp_sigma[indx[0]:indx[1]],zs=zs,ns=ns_i)
         x= p_zs>1.e-10
         zs_bins[i]['z']=zs[x]
-        zs_bins[i]['dz']=np.gradient(zs_bins[i]['z']) if len(zs_bins[i]['z'])>1 else 1
+        zs_bins[i]['dz']=tc_gradient(zs_bins[i]['z']) if len(zs_bins[i]['z'])>1 else 1
         zs_bins[i]['nz']=nz[x]
         zs_bins[i]['W']=1.
         zs_bins[i]['pz']=p_zs[x]*zs_bins[i]['W']
         zs_bins[i]['pzdz']=zs_bins[i]['pz']*zs_bins[i]['dz']
-        zs_bins[i]['Norm']=np.sum(zs_bins[i]['pzdz'])
-        sc=1./lu.sigma_crit(zl=zl_kernel,zs=zs[x],cosmo_h=cosmo_h)
-        zs_bins[i]['lens_kernel']=np.dot(zs_bins[i]['pzdz'],sc)
+        zs_bins[i]['Norm']=zs_bins[i]['pzdz'].sum()
+        # sc=1./lu.sigma_crit(zl=zl_kernel,zs=zs[x],cosmo_h=cosmo_h)
+        # zs_bins[i]['lens_kernel']=np.dot(zs_bins[i]['pzdz'],sc)
     zs_bins['n_bins']=nz_bins #easy to remember the counts
     zs_bins['z_lens_kernel']=zl_kernel
     return zs_bins
@@ -114,7 +113,7 @@ def lens_wt_tomo_bins(zp=None,p_zp=None,nz_bins=None,ns=26,ztrue_func=None,zp_bi
     if nz_bins is None:
         nz_bins=1
 
-    z_bins=np.linspace(min(zp),max(zp)*0.9,nz_bins) if z_bins is None else z_bins
+    z_bins=tc.linspace(min(zp),max(zp)*0.9,nz_bins) if z_bins is None else z_bins
 
     zs_bins0=source_tomo_bins(zp=zp,p_zp=p_zp,zp_bias=zp_bias,zp_sigma=zp_sigma,ns=ns,nz_bins=1)
     lu=Lensing_utils()
@@ -127,7 +126,7 @@ def lens_wt_tomo_bins(zp=None,p_zp=None,nz_bins=None,ns=26,ztrue_func=None,zp_bi
     dzs=zs_bins0[0]['dz']
     zs_bins={}
 
-    zl=np.linspace(0,2,50)
+    zl=tc.linspace(0,2,50)
     sc=1./lu.sigma_crit(zl=zl,zs=zs,cosmo_h=cosmo_h)
     scW=1./np.sum(sc,axis=1)
 

wigner_functions.py
@@ -1,36 +1,48 @@
 import numpy as np
+import torch as tc
 from scipy.special import binom,jn,loggamma
 from scipy.special import eval_jacobi as jacobi
 from multiprocessing import Pool,cpu_count
+from torch.multiprocessing import Pool as t_Pool
 from functools import partial
 
+def tc_binom(n,k):
+    return tc.exp(tc.lgamma(n+1)-tc.lgamma(k+1)-tc.lgamma(n-k+1))
+
 def wigner_d(m1,m2,theta,l):
-    k=np.amin([l-m1,l-m2,l+m1,l+m2],axis=0)
-    a=np.absolute(m1-m2)
+    k=tc.min(tc.stack((l-m1,l-m2,l+m1,l+m2)),0)[0]
+    a=tc.abs(tc.tensor(m1-m2,dtype=l.dtype,device=l.device))
     lamb=0 #lambda
     if m2>m1:
         lamb=m2-m1
     b=2*l-2*k-a
-    d_mat=(-1)**lamb
-    d_mat*=np.sqrt(binom(2*l-k,k+a)) #this gives array of shape l with elements choose(2l[i]-k[i], k[i]+a)
-    d_mat/=np.sqrt(binom(k+b,b))
-    d_mat=np.atleast_1d(d_mat)
+    #d_mat=(-1)**lamb
+    d_mat=tc.sqrt(tc_binom(2*l-k,k+a)) #this gives array of shape l with elements choose(2l[i]-k[i], k[i]+a)
+    d_mat*=(-1)**lamb
+    d_mat/=tc.sqrt(tc_binom(k+b,b))
+#     d_mat=tc.atleast_1d(d_mat)
     x=k<0
     d_mat[x]=0
-
+    d_mat=tc.tensor(d_mat,dtype=l.dtype,device=l.device)
+    if  d_mat.dim()==0:
+        d_mat=tc.tensor([d_mat],dtype=l.dtype,device=l.device)
     d_mat=d_mat.reshape(1,len(d_mat))
     theta=theta.reshape(len(theta),1)
-    d_mat=d_mat*((np.sin(theta/2.0)**a)*(np.cos(theta/2.0)**b))
-    d_mat*=jacobi(l,a,b,np.cos(theta))
+    d_mat=d_mat*((tc.sin(theta/2.0)**a)*(tc.cos(theta/2.0)**b))
+    d_mat*=tc.tensor(jacobi(l,a,b,tc.cos(theta)),dtype=l.dtype,device=l.device)
     return d_mat
 
 
 def wigner_d_parallel(m1,m2,theta,l,ncpu=None):
     if ncpu is None:
         ncpu=cpu_count()
-    p=Pool(ncpu)
-    d_mat=np.array(p.map(partial(wigner_d,m1,m2,theta),l))
-    return d_mat[:,:,0].T
+    # p=t_Pool(ncpu)
+    with Pool(ncpu) as p: #t_pool
+        d_mat=p.map(partial(wigner_d,m1,m2,theta),l)
+    # d_mat=tc.stack([d_mat[i][:,0] for i in np.arange(len(l))])
+    d_mat=d_mat[:,:,0].T
+    #p.close()
+    return d_mat
 
 def log_factorial(n):
     return loggamma(n+1)

wigner_transform.py
@@ -3,6 +3,7 @@ from scipy.interpolate import interp1d,interp2d,RectBivariateSpline
 from scipy.optimize import fsolve
 from wigner_functions import *
 import numpy as np
+import torch as tc
 import itertools
 
 class wigner_transform():
@@ -10,7 +11,7 @@ class wigner_transform():
         self.name='Wigner'
         self.logger=logger
         self.l=l
-        self.grad_l=np.gradient(l)
+        self.grad_l=tc.as_tensor(np.gradient(l),dtype=l.dtype,device=l.device)
         self.norm=(2*l+1.)/(4.*np.pi) #ignoring some factors of -1,
                                                     #assuming sum and differences of m1,m2
                                                     #are even for all correlations we need.
@@ -20,8 +21,7 @@ class wigner_transform():
         self.theta={}
         # self.theta=theta
         for (m1,m2) in m1_m2:
-            self.wig_d[(m1,m2)]=wigner_d_parallel(m1,m2,theta,self.l,ncpu=ncpu)
-            # self.wig_d[(m1,m2)]*=self.norm #this works for covariance and correlation function
+            self.wig_d[(m1,m2)]=wigner_d(m1,m2,theta,self.l,)#ncpu=ncpu) #FIXME: Use the parallel version
             self.theta[(m1,m2)]=theta #FIXME: Ugly
 
 
@@ -64,9 +64,16 @@ class wigner_transform():
             m1_m2_cross=m1_m2
         #when cl_cov can be written as vector, eg. gaussian covariance
         cl2=self.cl_grid(l_cl=l_cl,cl=cl_cov,taper=taper,**kwargs)
-        cov=np.einsum('rk,k,sk->rs',self.wig_d[m1_m2]*np.sqrt(self.norm),cl2*self.grad_l,
-                    self.wig_d[m1_m2_cross]*np.sqrt(self.norm),optimize=True)
+        cov=(self.wig_d[m1_m2]*np.sqrt(self.norm))@(
+            (self.wig_d[m1_m2_cross]*np.sqrt(self.norm)*cl2*self.grad_l))
+        #@==np.matmul
+#         cov=np.einsum('rk,k,sk->rs',self.wig_d[m1_m2]*np.sqrt(self.norm),cl2*self.grad_l,
+#                     self.wig_d[m1_m2_cross]*np.sqrt(self.norm),optimize=True)
         #FIXME: Check normalization
+
+#         test case:%time xx=np.einsum('rk,k,sk->rs',x2,x1,x2)
+#                     %time xx2=x2@(x2*x1).T
+
         return self.theta[m1_m2],cov
 
     def projected_covariance2(self,l_cl=[],cl_cov=[],m1_m2=[],m1_m2_cross=None,
@@ -75,7 +82,10 @@ class wigner_transform():
         if m1_m2_cross is None:
             m1_m2_cross=m1_m2
         cl_cov2=cl_cov#self.cl_cov_grid(l_cl=l_cl,cl_cov=cl_cov,m1_m2=m1_m2,taper=taper,**kwargs)
-        cov=np.dot(self.wig_d[m1_m2]*self.grad_l*np.sqrt(self.norm),np.dot(self.wig_d[m1_m2_cross]*np.sqrt(self.norm),cl_cov2).T)
+#         cov=np.dot(self.wig_d[m1_m2]*self.grad_l*np.sqrt(self.norm),
+#                    np.dot(self.wig_d[m1_m2_cross]*np.sqrt(self.norm),cl_cov2).T)
+        cov=(self.wig_d[m1_m2]*self.grad_l*np.sqrt(self.norm))@cl_cov2@(
+                                    self.wig_d[m1_m2_cross]*np.sqrt(self.norm)).T
         # cov*=self.norm
         #FIXME: Check normalization
         return self.theta[m1_m2],cov