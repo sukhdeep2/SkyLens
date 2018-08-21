7666e61b76a98b61d8414d01362f41e1d0821234
deep <deep@genie>
2018-08-21 14:13:05 -0700

    temporary fix to the wigner issues

--- wigner_functions.py
+++ wigner_functions.py
@@ -4,7 +4,16 @@ from scipy.special import eval_jacobi as jacobi
 from multiprocessing import Pool,cpu_count
 from functools import partial
 
-def wigner_d(m1,m2,theta,l):
+def wigner_d(m1,m2,theta,l,l_use_bessel=1.e4):
+    l0=np.copy(l)
+    if l_use_bessel is not None:
+    #FIXME: This is not great. Due to a issues with the scipy hypergeometric function, jacobi can output nan for large ell, l>1.e4
+    # As a temporary fix, for ell>1.e4, we are replacing the wigner function with the bessel function. Fingers and toes crossed!!!
+    # mpmath is slower and also has convergence issues at large ell. 
+    #https://github.com/scipy/scipy/issues/4446
+        l=np.atleast_1d(l)
+        x=l<l_use_bessel
+        l=np.atleast_1d(l[x])
     k=np.amin([l-m1,l-m2,l+m1,l+m2],axis=0)
     a=np.absolute(m1-m2)
     lamb=0 #lambda
@@ -22,14 +31,21 @@ def wigner_d(m1,m2,theta,l):
     theta=theta.reshape(len(theta),1)
     d_mat=d_mat*((np.sin(theta/2.0)**a)*(np.cos(theta/2.0)**b))
     d_mat*=jacobi(l,a,b,np.cos(theta))
+    
+    if l_use_bessel is not None:
+        l=np.atleast_1d(l0)
+        x=l>=l_use_bessel
+        l=np.atleast_1d(l[x])
+#         d_mat[:,x]=jn(m1-m2,l[x]*theta)
+        d_mat=np.append(d_mat,jn(m1-m2,l*theta),axis=1)
     return d_mat
 
 
-def wigner_d_parallel(m1,m2,theta,l,ncpu=None):
+def wigner_d_parallel(m1,m2,theta,l,ncpu=None,l_use_bessel=1.e4):
     if ncpu is None:
         ncpu=cpu_count()
     p=Pool(ncpu)
-    d_mat=np.array(p.map(partial(wigner_d,m1,m2,theta),l))
+    d_mat=np.array(p.map(partial(wigner_d,m1,m2,theta,l_use_bessel=l_use_bessel),l))
     return d_mat[:,:,0].T
 
 def log_factorial(n):
