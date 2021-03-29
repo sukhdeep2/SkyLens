fixed correlation function covariance

scripts/sim_args.py
@@ -18,21 +18,21 @@ lognormal_scale=2
 
 nside=1024
 do_pseudo_cl=True
-do_xi= True #not do_pseudo_cl
+do_xi= False #not do_pseudo_cl
 
 if do_xi:
     nside=512
 
 #cl args
 lmax_cl=nside#
-Nl_bins=37 #40
+Nl_bins=25 #40
 lmin_cl=0
 l0=np.arange(lmin_cl,lmax_cl)
 
 lmin_cl_Bins=lmin_cl+10
 lmax_cl_Bins=lmax_cl-10 #this reduces the bias from ell cutoff in p-cl
 # l_bins=np.int64(np.logspace(np.log10(lmin_cl_Bins),np.log10(lmax_cl_Bins),Nl_bins))
-l_bins=get_l_bins(l_min=lmin_cl_Bins,l_max=lmax_cl_Bins,N_bins=Nl_bins,binning_scheme='log',min_modes=500)
+l_bins=get_l_bins(l_min=lmin_cl_Bins,l_max=lmax_cl_Bins,N_bins=Nl_bins,binning_scheme='log',min_modes=50)
 lb=(l_bins[1:]+l_bins[:-1])*.5
 
 l=l0 #np.unique(np.int64(np.logspace(np.log10(lmin_cl),np.log10(lmax_cl),Nl_bins*20))) #if we want to use fewer ell

skylens/cov_utils.py
@@ -23,13 +23,14 @@ class Covariance_utils():
                  use_binned_l=False,xi_SN_analytical=False,SN={},use_binned_theta=False,
                  do_cov=False,SSV_cov=False,tidal_SSV_cov=False,WT=None, #WT_binned_cov={},
                  Tri_cov=False,sparse_cov=False,cl_bin_utils=None,xi_bin_utils=None,
-                 xi_win_approx=True,do_pseudo_cl=True,bin_cl=True
+                 do_pseudo_cl=True,bin_cl=True
                 ):
         self.__dict__.update(locals()) #assign all input args to the class as properties
         self.binning=binning()
         self.sample_variance_f=1
-        if not do_sample_variance:
-            self.sample_variance_f=0 #remove sample_variance from gaussian part
+        if do_sample_variance is not None:
+            if not do_sample_variance:
+                self.sample_variance_f=0 #remove sample_variance from gaussian part
 
         self.set_window_params()
 
@@ -324,7 +325,7 @@ class Covariance_utils():
                 fs1324=f_sky[tracers][z_indx]
                 fs0=f_sky[tracers[0],tracers[1]][z_indx[0],z_indx[1]] * f_sky[tracers[2],tracers[3]][z_indx[2],z_indx[3]]
                 fs1423=f_sky[tracers][z_indx]
-            Norm=Norm*fs0/fs1324
+            Norm=Norm*fs0/fs1324 #FIXME: This is an approximation. Need better expression for correlation functions
 
         
         for corr_i in [1324,1423]:
@@ -332,7 +333,7 @@ class Covariance_utils():
             c1=cv_indxs[corr_i][0]
             c2=cv_indxs[corr_i][1]
 
-            for k in Win['xi'][12].keys():
+            for k in Win['xi_cov'][corr_i].keys():
                 for wp in W_pm:
                     CV2=CV
                     for a_EB in np.arange(add_EB):
@@ -359,7 +360,9 @@ class Covariance_utils():
 #                             th,G_t=WT.projected_covariance2(cl_cov=G_t,**WT_kwargs)
                             th,G_t=self.WT.projected_covariance(cl_cov=G_t,**WT_kwargs)
                         if Win is not None:
-                            G_t*=np.outer(Win['xi'][12][k],Win['xi'][34][k])
+#                             G_t*=np.outer(Win['xi'][12][k],Win['xi'][34][k])
+                            G_t*=Win['xi_cov'][corr_i][k]
+                            
                         G_t/=Norm
 
                         if a_EB>0:
@@ -536,7 +539,7 @@ def cl_cov(zs_indx,CU,z_bins=None,sig_cL=None,cls=None, tracers=[],Win_cov=None,
                                         tracers,zs_indx,Win,)
     else:
         fs=self.f_sky
-        if self.do_xi and self.xi_win_approx and self.use_window : #in this case we need to use a separate function directly from xi_cov
+        if self.do_xi and self.use_window : #in this case we need to use a separate function directly from xi_cov
             cov['G1324']=0
             cov['G1423']=0
         else:
@@ -605,16 +608,11 @@ def xi_cov(cov_indx,CU,cov_cl=None,cls={},s1_s2=None,s1_s2_cross=None,
     if self.use_binned_l:
         wig_d1=WT_kwargs['wig_d1']
         wig_d2=WT_kwargs['wig_d2']
-#             wig_d1=WT_binned_cov[corr1][s1_s2][indxs_1]
-#             wig_d2=self.WT_binned_cov[corr2][s1_s2_cross][indxs_2]
 
     Win=None
-#         if self.use_window and self.store_win:
-#             Win_cov=self.Win.Win['cov'][tracers]
     if Win_cov is not None:
         Win=Win_cov#[z_indx]
 
-#         WT_kwargs={'l_cl':self.l,'s1_s2':s1_s2,'s1_s2_cross':s1_s2_cross,'wig_d1':wig_d1,'wig_d2':wig_d2}
     bf=1
     if np.all(np.array(tracers)=='shear') and not s1_s2==s1_s2_cross: #cross between xi+ and xi-
         bf=-1
@@ -649,9 +647,10 @@ def xi_cov(cov_indx,CU,cov_cl=None,cls={},s1_s2=None,s1_s2_cross=None,
             cov_xi['Tri']=self.binning.bin_2d(cov=cov_xi['Tri'],bin_utils=xi_bin_utils)
 
     cov_xi['final']=cov_xi['G']+cov_xi['SSC']+cov_xi['Tri']
-    if self.use_window and self.xi_win_approx:
+    if self.use_window:
         cov_xi['G']/=(Win_cl1['xi_b']*Win_cl2['xi_b'])
-        cov_xi['final']/=(Win_cl1['xi_b']*Win_cl2['xi_b'])
+        cov_xi['final']=cov_xi['G']+cov_xi['SSC']+cov_xi['Tri']
+#         cov_xi['final']/=(Win_cl1['xi_b']*Win_cl2['xi_b'])#FIXME: SSC is not multiplied with window.
 
     if self.sparse_cov:
         for k in ['G','SSC','Tri','final']:

skylens/survey_utils.py
@@ -79,8 +79,8 @@ def ztrue_given_pz_Gaussian(zp=[],p_zp=[],bias=[],sigma=[],zs=None):
 def set_window(shear_zbins={},f_sky=0.3,nside=256,mask_start_pix=0,window_cl_fact=None,unit_win=False,scheduler_info=None,mask=None,
               delta_W=True):
     from skylens.skylens_main import Skylens
-    w_lmax=3*nside-1
-    l0=np.arange(3*nside-1,dtype='int')
+    w_lmax=3*nside
+    l0=np.arange(w_lmax,dtype='int')
     corr=('galaxy','galaxy')
     
     kappa0=Skylens(galaxy_zbins=shear_zbins,do_cov=False,bin_cl=False,l_bins=None,l=l0,use_window=False,

skylens/window_utils.py
@@ -1175,9 +1175,7 @@ def get_window_power_cl(corr_indxs,WU,c_ell0=None,c_ell_b=None,z_bin1=None,z_bin
     win['M']={} #self.coupling_matrix_large(win['cl'], s1s2,wig_3j_2=wig_3j_2,W_pm=W_pm)*(2*self.l[:,None]+1) #FIXME: check ordering
     win['M_noise']=None
     return win
-
-
-
+    
 def get_window_power_cov(corr_indxs,WU,bin_wt_cl=None,bin_wt_xi=None,z_bins={},
                         WT_kwargs=None,xi_bin_utils=None):#corr1=None,corr2=None,indxs1=None,indxs2=None):
     """
@@ -1311,11 +1309,16 @@ def get_window_power_cov(corr_indxs,WU,bin_wt_cl=None,bin_wt_xi=None,z_bins={},
     win['f_sky12'],mask12=self.mask_comb(z_bin1['window'],z_bin2['window'],
                                  )#For SSC
     win['f_sky34'],mask34=self.mask_comb(z_bin3['window'],z_bin4['window']
-                                 )
+                                     )
+    win['f_sky1234'],mask1234=self.mask_comb(mask12,mask34)
+    
     win['mask_comb_cl']=hp.anafast(map1=mask12,
                              map2=mask34,
                              lmax=self.window_lmax
                         ) #based on 4.34 of https://arxiv.org/pdf/1711.07467.pdf
+    win['mask_comb_cl1234']=hp.anafast(map1=mask1234,
+                             lmax=self.window_lmax
+                        )
     win['Om_w12']=win['f_sky12']*4*np.pi
     win['Om_w34']=win['f_sky34']*4*np.pi
     del mask12,mask34
@@ -1325,90 +1328,118 @@ def get_window_power_cov(corr_indxs,WU,bin_wt_cl=None,bin_wt_xi=None,z_bins={},
         win['M'][1324][k]={wp:{} for wp in W_pm[1324]}
     for k in win[1423].keys():
         win['M'][1423][k]={wp:{} for wp in W_pm[1423]}
-
+        
     win['xi']={12:{},34:{}}
+    win['xi_cov']={1324:{},1423:{}}
     win['xi_b']={12:{},34:{}}
     win['xi_b_th']={12:{},34:{}}
     cl12={}
     cl34={}
+
     if self.do_xi:
+        for k in cl12.keys():
+            th,win['xi'][12][k]=self.WT.projected_correlation(cl=cl12[k],**WT_kwargs)
+            th,win['xi'][34][k]=self.WT.projected_correlation(cl=cl34[k],**WT_kwargs)
+            if self.bin_theta_window:
+                win['xi'][12][k]=self.binning.bin_1d(xi=win['xi'][12][k],bin_utils=xi_bin_utils)
+                win['xi'][34][k]=self.binning.bin_1d(xi=win['xi'][34][k],bin_utils=xi_bin_utils)
+        th,win['xi_cov']['mask1234']=self.WT.projected_covariance(cl_cov=win['mask_comb_cl1234'],**WT_kwargs)
+        for cov_indx in win['xi_cov'].keys():
+            if cov_indx==1324:
+                i1=(0,2)
+                i2=(1,3)
+            else:
+                i1=(0,3)
+                i2=(1,2)
+            window_xis={}
+            
+            z_wins={}
             k='clcl'
-            cl12[k]=hp.anafast(map1=z_bin1['window'],map2=z_bin2['window'],lmax=self.window_lmax
-                    )[self.window_l]
-            cl34[k]=hp.anafast(map1=z_bin3['window'],map2=z_bin4['window'],lmax=self.window_lmax
-                    )[self.window_l]
-            if corr[0]==corr[2] and indxs[0]==indxs[2]: #noise X cl
+            z_wins[1]=z_bin1['window']*1
+            z_wins[2]=z_bin2['window']*1
+            z_wins[3]=z_bin3['window']*1
+            z_wins[4]=z_bin4['window']*1
+            window_xis[k]=xi_cov_window(WU=self,WT_kwargs=WT_kwargs,mask_xi=win['xi_cov']['mask1234'],z_windows=z_wins)
+
+            if corr[i1[0]]==corr[i1[1]] and indxs[i1[0]]==indxs[i1[1]]: #noise X cl
                 k='Ncl'
-                m1=z_bin1['window_N']
-                m3=z_bin3['window_N']
-#                 m1=np.sqrt(z_bin1['window'])
-#                 m3=np.sqrt(z_bin3['window'])
-#                 m1[z_bin1['window']==hp.UNSEEN]=hp.UNSEEN
-#                 m3[z_bin3['window']==hp.UNSEEN]=hp.UNSEEN
-                cl12[k]=hp.anafast(map1=m1,map2=z_bin2['window'],lmax=self.window_lmax
-                        )[self.window_l]
-                cl34[k]=hp.anafast(map1=m3,map2=z_bin4['window'],lmax=self.window_lmax
-                        )[self.window_l]
-            if corr[1]==corr[3] and indxs[1]==indxs[3]:#noise X cl
+                z_wins[1]=z_bin1['window_N']*1
+                z_wins[2]=z_bin2['window']*1
+                z_wins[3]=z_bin3['window_N']*1
+                z_wins[4]=z_bin4['window']*1
+                window_xis[k]=xi_cov_window(WU=self,WT_kwargs=WT_kwargs,mask_xi=win['xi_cov']['mask1234'],z_windows=z_wins)
+            if corr[i2[0]]==corr[i2[1]] and indxs[i2[0]]==indxs[i2[1]]:#noise X cl
                 k='clN'
-                m2=z_bin2['window_N']
-                m4=z_bin4['window_N']
-#                 m2=np.sqrt(z_bin2['window'])
-#                 m4=np.sqrt(z_bin4['window'])
-#                 m2[z_bin2['window']==hp.UNSEEN]=hp.UNSEEN
-#                 m4[z_bin4['window']==hp.UNSEEN]=hp.UNSEEN
-                cl12[k]=hp.anafast(map1=m2,map2=z_bin1['window'],lmax=self.window_lmax
-                        )[self.window_l]
-                cl34[k]=hp.anafast(map1=m4,map2=z_bin3['window'],lmax=self.window_lmax
-                        )[self.window_l]
-            if corr[0]==corr[2] and indxs[0]==indxs[2] and corr[1]==corr[3] and indxs[1]==indxs[3]: #noise X noise
+                z_wins[1]=z_bin1['window']*1
+                z_wins[2]=z_bin2['window_N']*1
+                z_wins[3]=z_bin3['window']*1
+                z_wins[4]=z_bin4['window_N']*1
+                window_xis[k]=xi_cov_window(WU=self,WT_kwargs=WT_kwargs,mask_xi=win['xi_cov']['mask1234'],z_windows=z_wins)
+
+            if corr[i1[0]]==corr[i1[1]] and indxs[i1[0]]==indxs[i1[1]] and corr[i2[0]]==corr[i2[1]] and indxs[i2[0]]==indxs[i2[1]]: #noise X noise
                 k='NN'
-                m2=z_bin2['window_N']
-                m4=z_bin4['window_N']
-                m1=z_bin1['window_N']
-                m3=z_bin3['window_N']
-
-#                 m2=np.sqrt(z_bin2['window'])
-#                 m4=np.sqrt(z_bin4['window'])
-#                 m1=np.sqrt(z_bin1['window'])
-#                 m3=np.sqrt(z_bin3['window'])
-#                 m1[z_bin1['window']==hp.UNSEEN]=hp.UNSEEN
-#                 m3[z_bin3['window']==hp.UNSEEN]=hp.UNSEEN
-#                 m2[z_bin2['window']==hp.UNSEEN]=hp.UNSEEN
-#                 m4[z_bin4['window']==hp.UNSEEN]=hp.UNSEEN
-                cl12[k]=hp.anafast(map1=m2,map2=m1,lmax=self.window_lmax
-                        )[self.window_l]
-                cl34[k]=hp.anafast(map1=m4,map2=m3,lmax=self.window_lmax
-                        )[self.window_l]
-            for k in cl12.keys():
-                th,win['xi'][12][k]=self.WT.projected_correlation(cl=cl12[k],**WT_kwargs)
-                th,win['xi'][34][k]=self.WT.projected_correlation(cl=cl34[k],**WT_kwargs)
-                if self.bin_theta_window:
-                    win['xi'][12][k]=self.binning.bin_1d(xi=win['xi'][12][k],bin_utils=xi_bin_utils)
-                    win['xi'][34][k]=self.binning.bin_1d(xi=win['xi'][34][k],bin_utils=xi_bin_utils)
-#             if self.bin_theta_window: #FIXME: Following NEEDS fixing
-#                 for s1 in self.s1_s2s[corr1]:
-#                     for s2 in self.s1_s2s[corr2]:
-#                         bin_wt_xi={}
-#                         bin_wt_xi['xi12']=xi0[corr1][s1][indxs1]
-#                         bin_wt_xi['xi34']=xi0[corr2][s2][indxs2]
-#                         bin_wt_xi['xi_b12']=xi_b[corr1][s1][indxs1]
-#                         bin_wt_xi['xi_b34']=xi_b[corr2][s2][indxs2]
-
-
-#                         bin_wt_xi1324={'wt0':np.sqrt(bin_wt_xi['xi12']*bin_wt_xi['xi34'])} 
-#                         bin_wt_xi1324['wt_b']=1./np.sqrt(bin_wt_xi['xi_b12']*bin_wt_xi['xi_b34'])
-#                         bin_wt_xi1423=bin_wt_xi1324 #{'wt0':np.sqrt(win['bin_wt']['cl14']*win['bin_wt']['cl23'])} 
-# #                         bin_wt_xi1423['wt_b']=1./np.sqrt(win['bin_wt']['cl_b14']*win['bin_wt']['cl_b23'])
-#                         win['xi_b_th'][1324]['bin_wt_xi1324']=bin_wt_xi1324
-#                         win['xi_b_th'][1423]['bin_wt_xi1423']=bin_wt_xi1423
-#                         for k in win[1324].keys():
-#                             win['xi_b_th'][1324][k][s1+s2]=self.binnings.bin_2d_coupling(M=win['xi'][1324][k],bin_utils=self.xi_bin_utils[s1],
-#                             partial_bin_side=None,lm=None,lm_step=None,wt0=bin_wt_xi1324['wt0'],wt_b=bin_wt_xi1324['wt_b'],cov=False)
-#                         for k in win[1423].keys():
-#                             win['xi_b_th'][1423][k][s1+s2]=self.binnings.bin_2d_coupling(M=win['xi'][1423][k],bin_utils=self.xi_bin_utils[s1],
-#                             partial_bin_side=None,lm=None,lm_step=None,wt0=bin_wt_xi1423['wt0'],wt_b=bin_wt_xi1423['wt_b'],cov=False)
+                z_wins[1]=z_bin1['window_N']*1
+                z_wins[2]=z_bin2['window_N']*1
+                z_wins[3]=z_bin3['window_N']*1
+                z_wins[4]=z_bin4['window_N']*1
+                window_xis[k]=xi_cov_window(WU=self,WT_kwargs=WT_kwargs,mask_xi=win['xi_cov']['mask1234'],z_windows=z_wins)
+                
+            if self.bin_theta_window: #FIXME: wrong binning order
+                for k in window_xis.keys():
+                    window_xis[k]=self.binning.bin_2d(cov=window_xis[k],bin_utils=xi_bin_utils)
+            win['xi_cov'][cov_indx]=window_xis
 
     win['W_pm']=W_pm
     win['s1s2']=s1s2s
     return win
+
+def xi_cov_window(WU,WT_kwargs={},mask_xi=[],z_windows={}):
+    xis=mask_xi*1.
+#     return xis #FIXME: following takes too long and doesn't make much of a difference. Still needs to be stress tested.
+    self=WU
+    masks={}
+    for i in z_windows.keys():
+        mask=z_windows[i]==hp.UNSEEN
+        z_windows[i]-=z_windows[i][~mask].mean()
+        z_windows[i][mask]=hp.UNSEEN
+        masks[i]=mask
+    mls={}
+    cls={}
+#     for i in z_windows.keys():
+#         for j in z_windows.keys():
+#             if i<=j:
+#                 continue
+#             cls[(i,j)]=hp.anafast(map1=z_windows[i],map2=z_windows[j],lmax=self.window_lmax
+#                     )[self.window_l]
+#             cls[(j,i)]=cls[(i,j)]
+#             mls[(i,j)]=hp.anafast(map1=masks[i],map2=masks[j],lmax=self.window_lmax
+#                     )[self.window_l]
+#             mls[(j,i)]=mls[(i,j)]
+
+    bin_indxs=[(1,2,3,4),(1,3,2,4),(1,4,2,3)]
+    for (i,j,k,l) in bin_indxs:
+        cli=window_4_cl(z_windows[i],z_windows[j],masks[k],masks[l])[self.window_l]
+        thi,xit=self.WT.projected_covariance(cl_cov=cli,**WT_kwargs)
+        xis+=xit
+        
+        clj=window_4_cl(z_windows[k],z_windows[l],masks[i],masks[j])[self.window_l]
+        thi,xit=self.WT.projected_covariance(cl_cov=clj,**WT_kwargs)
+        xis+=xit
+        
+#         cli=cls[(i,j)]*cls[(k,l)]
+        cli=cli*clj
+        thi,xit=self.WT.projected_covariance(cl_cov=cli,**WT_kwargs)
+        xis+=xit
+    return xis
+
+def window_4_cl(window1,window2,mask3,mask4):
+    z_windows_i=window1*1
+    z_windows_i[mask3]=hp.UNSEEN
+    z_windows_i[mask4]=hp.UNSEEN
+
+    z_windows_j=window2*1
+    z_windows_j[mask3]=hp.UNSEEN
+    z_windows_j[mask4]=hp.UNSEEN
+
+    cli=hp.anafast(map1=z_windows_i,map2=z_windows_j)
+    return cli