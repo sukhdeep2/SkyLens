07e47a190effadb4f1e9ff79d098206ef5d0ecdb
Tanveer <tanveer.karim@cfa.harvard.edu>
2021-04-12 19:28:17 +0000

    update pk_params_default to run class_pk

--- skylens/power_spectra.py
+++ skylens/power_spectra.py
@@ -42,7 +42,7 @@ cosmo_fid=dict({'h':cosmo.h,'Omb':cosmo.Ob0,'Omd':cosmo.Om0-cosmo.Ob0,'s8':0.817
                 'Ase9':2.2,'mnu':cosmo.m_nu[-1].value,'Omk':cosmo.Ok0,'tau':0.06,'ns':0.965,
                 'OmR':cosmo.Ogamma0+cosmo.Onu0,'w':-1,'wa':0,'Tcmb':cosmo.Tcmb0,'z_max':4,'use_astropy':True})
 cosmo_fid['Oml']=1.-cosmo_fid['Om']-cosmo_fid['Omk']
-pk_params_default={'non_linear':1,'kmax':30,'kmin':3.e-4,'nk':500,'scenario':'dmo','pk_func':'camb_pk_too_many_z','halofit_version':'takahashi'}
+pk_params_default={'non_linear':1,'kmax':30,'kmin':3.e-4,'nk':500,'scenario':'dmo','pk_func':'class_pk','halofit_version':'takahashi'}
 
 # baryonic scenario option:
 # "owls_AGN","owls_DBLIMFV1618","owls_NOSN","owls_NOSN_NOZCOOL","owls_NOZCOOL","owls_REF","owls_WDENS"
@@ -107,9 +107,10 @@ class Power_Spectra(cosmology):
         super().__init__(cosmo_params=cosmo_params)
         self.name='PS'
         pk_func=pk_params.get('pk_func')
-        pk_func_default=self.camb_pk_too_many_z
-        if classPk:
-            pk_func_default=self.class_pk
+        #pk_func_default=self.camb_pk_too_many_z
+        pk_func_default=self.class_pk
+        #if classPk:
+        #    pk_func_default=self.class_pk
         print('power spectra',pk_func)
         self.pk_func=pk_func_default if pk_func is None else getattr(self,pk_func)
         if not pk_params is None:
